bcaf573fd4d807a925798886e435de82a88e3c8c
omar2500-mAh <o.faruque.iu2022@gmail.com>
2026-06-15 13:39:27 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/_Portfolio">
       <DataProvider>
         <App />
       </DataProvider>
